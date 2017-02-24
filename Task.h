f5f10dad68cf81822293ab6317aaa3c84939f100
ethan <foolishantcat@gmail.com>
2017-02-24 22:53:19 +0800

    first commit

---
+++ Task.h
@@ -0,0 +1,36 @@
+#ifndef _TASK_H_
+#define _TASK_H_
+
+namespace 
+{
+    enum PRIORITY
+    {
+        MIN = 1, 
+        NORMAL = 25,
+        MAX = 50
+    };
+}
+
+class Task
+{
+public:
+    Task()
+    {
+    
+    }
+    void SetPriority(int priority)
+    {
+        if(priority > (PRIORITY::MAX))
+        {
+            priority = (PRIORITY::MAX);
+        }
+        else if(priority > (PRIORITY::MAX))
+        {
+            priority = (PRIORITY::MIN);
+        }
+    }
+    virtual void Run() = 0;
+protected:
+    int priority_;
+};
+    
